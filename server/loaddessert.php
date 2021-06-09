34f3c4a656a80c2eca6f7e4beca0f2c96974fef4
lowchiaqi1234 <80511480+lowchiaqi1234@users.noreply.github.com>
2021-06-09 21:06:33 +0800

    PHP

---
+++ server/loaddessert.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once("dbconnect.php");
+
+$sqlloadproduct = "SELECT * FROM tbl_products WHERE prtype='Dessert' ORDER BY prid DESC";
+$result = $conn->query($sqlloadproduct);
+
+if ($result->num_rows > 0)
+{
+    $response["products"] = array();
+    while ($row = $result->fetch_assoc())
+    {
+        $productlist = array();
+        $productlist[prid] = $row["prid"];
+        $productlist[prname] = $row["prname"];
+        $productlist[prtype] = $row["prtype"];
+        $productlist[prprice] = $row["prprice"];
+        $productlist[prqty] = $row["prqty"];
+        array_push($response["products"], $productlist);
+    }
+    echo json_encode($response);
+}
+else
+{
+    echo "nodata";
+}
+?>
